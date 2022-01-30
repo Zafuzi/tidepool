b0a323e8e6a47a59c6b878129c9b31af72d6e8fc
Zafuzi <zacharyfoutz@gmail.com>
2022-01-30 12:01:14 -0800

    updated mkdist for butler

---
+++ mkdist.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ " $@ " =~ "-c " ]]; then
+	echo "removing old 'dist' ..."
+	rm -rf dist
+	rm dist.zip
+fi
+
+
+# echo "creating 'dist' ..."
+mkdir -p dist
+
+cp index.html dist
+cp *.js dist
+cp -rL data dist
+cp -rL lib dist
+cp -rL nodes dist
+
+echo "minifying dist/game.js"
+node_modules/uglify-js/bin/uglifyjs dist/game.js --compress --mangle --output dist/game.js;
+
+files=(dist/**/*.js)
+
+for file in ${files[*]}
+do
+	echo "minifying $file"
+	cat $file | node_modules/uglify-js/bin/uglifyjs --compress --mangle --output $file;
+done
+
+if [[ " $@ " =~ "-z " ]]; then
+	zip -r dist.zip dist;
+fi
+
+if [[ " $@ " =~ "-p " ]]; then
+	# edit this line to push to itch
+	butler push dist username/game_name:html5;
+	butler status username/game_name:html5
+fi
+
+if [[ " $@ " =~ "-s " ]]; then
+	butler status username/game_name:html5
+fi
--- nodes/slime.js
+++ nodes/slime.js
@@ -3,5 +3,9 @@ function slime()
 	load_image("data/slime1.png", function(slime_img)
 	{
 		let sq = new Squid(vec(100, 100), slime_img);
+		sq.listen("tick", function()
+		{
+			sq.position.x += 1;
+		});
 	});
 }
