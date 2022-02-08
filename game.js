296aa4c3177bc4afc61d86624cde44044df6afbe
Zafuzi <zacharyfoutz@gmail.com>
2022-02-08 10:30:07 -0800

    * added adaptive scaling

--- game.js
+++ game.js
@@ -1,26 +1,43 @@
-let screen = vec(800, 600);
+let screen = vec(1600, 900);
 canvas.width = screen.x;
 canvas.height = screen.y;
 
 document.addEventListener("DOMContentLoaded", function()
 {
+	canvas.style.width = "unset";
+	canvas.style.height = "unset";
+	canvas.style.top = "unset";
+	canvas.style.left = "unset";
 
 	let toggleDebug = newInputType("toggleDebug", ["`"]);
 
 	let brain = new Thing();
 		brain.listen("tick", function()
 		{
-			// determine which is smaller
-			if(window.innerWidth > window.innerHeight)
-			{
-				canvas.style.height = window.innerHeight + "px";
-				canvas.style.width = 4 * window.innerHeight / 3 + "px";
-			}
-			else
-			{
-				canvas.style.width = window.innerWidth + "px";
-				canvas.style.height = 3 * window.innerWidth / 4 + "px";
-			}
+			let image = {
+				x: 0, y: 0, width: screen.x, height: screen.y
+			};
+
+			let originalWidth = image.width;
+			let originalHeight = image.height;
+
+			let options = {
+				container: new AS.Size(window.innerWidth, window.innerHeight),
+				target: new AS.Size(originalWidth, originalHeight),
+				policy: AS.POLICY.ShowAll
+			};
+
+			let rect = AS.getScaledRect(options);
+
+			image.x = rect.x;
+			image.y = rect.y;
+			image.width = rect.width;
+			image.height = rect.height;
+
+			canvas.style.width = image.width + "px";
+			canvas.style.height = image.height + "px";
+			canvas.style.top = image.top + "px";
+			canvas.style.left = image.left + "px";
 
 			if(toggleDebug.pressed && input.debounce === 0)
 			{
--- nodes/slime.js
+++ nodes/slime.js
@@ -3,9 +3,30 @@ function slime()
 	load_image("data/slime1.png", function(slime_img)
 	{
 		let sq = new Squid(vec(100, 100), slime_img);
+			sq.speed = 4;
+			sq.velocity.x = -sq.speed;
+			sq.velocity.y = -sq.speed;
 		sq.listen("tick", function()
 		{
-			sq.position.x += 1;
+			if(sq.position.x + slime_img.size().x / 2 - 8 > screen.x)
+			{
+				sq.velocity.x = -sq.speed;
+			}
+
+			if(sq.position.x - 8 <= 0)
+			{
+				sq.velocity.x = sq.speed;
+			}
+
+			if(sq.position.y + slime_img.size().y / 2 > screen.y)
+			{
+				sq.velocity.y = -sq.speed;
+			}
+
+			if(sq.position.y - 12 <= 0)
+			{
+				sq.velocity.y = sq.speed;
+			}
 		});
 	});
 }
