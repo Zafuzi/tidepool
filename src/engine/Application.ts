d6af4d1eff472556a09eb235136844898af812ee
Zachary Harper <zachary@boomlearning.com>
2026-01-22 07:02:21 -0800

    removed Application.ts

--- src/engine/Application.ts
+++
@@ -1,9 +0,0 @@
-import { initDevtools } from "@pixi/devtools";
-import { Application } from "pixi.js";
-
-// Game container instance - initialized via init()
-
-export const App: Application = new Application();
-await initDevtools({
-	app: App,
-});
